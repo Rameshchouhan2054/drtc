03c5270d00dec9d95661911d8c40a197dbb2faed
rameshchouhan2054 <rameshchouhan2054@gmail.com>
2021-09-07 11:40:40 +0530

    first commit

---
+++ application/modules/Frontend/controllers/Frontend.php
@@ -0,0 +1,47 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+/**
+ * CodeIgniter-HMVC
+ *
+ * @package    CodeIgniter-HMVC
+ * @author     N3Cr0N (N3Cr0N@list.ru)
+ * @copyright  2019 N3Cr0N
+ * @license    https://opensource.org/licenses/MIT  MIT License
+ * @link       <URI> (description)
+ * @version    GIT: $Id$
+ * @since      Version 0.0.1
+ * @filesource
+ *
+ */
+
+class Frontend extends CI_Controller
+{
+    /**
+     * [__construct description]
+     *
+     * @method __construct
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+    }
+
+    /**
+     * [index description]
+     *
+     * @method index
+     *
+     * @return [type] [description]
+     */
+    public function index()
+    {
+        $this->load->view('index');
+        //$this->load->view('frontend/dashboard');
+    }
+    function aboutUs()
+    {
+        echo "hii";
+        $this->load->view('aboutUs');
+    }
+}
---
+++ application/modules/Frontend/controllers/Home.php
@@ -0,0 +1,33 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Home extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('User_model');
+    }
+
+    function index()
+    {
+        $this->load->view('index');
+    }
+
+    function aboutUs()
+    {
+        $this->load->view('aboutUs');
+    }
+
+    function our_services()
+    {
+        $this->load->helper('url');
+
+        $this->load->view('our_services');
+    }
+
+    function timeAndDistance()
+    {
+        $this->load->view('customer_care/timeAndDistance');
+    }
+}
